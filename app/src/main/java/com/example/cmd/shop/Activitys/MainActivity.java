798e7c7409a6c7255ef32293f400219acacec862
Kristjan Tampere <dacmd@mail.ru>
2017-10-20 16:39:21 +0300

    commit

--- app/src/main/java/com/example/cmd/shop/Activitys/MainActivity.java
+++ app/src/main/java/com/example/cmd/shop/Activitys/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.cmd.shop;
+package com.example.cmd.shop.Activitys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.cmd.shop.R;
 
 public class MainActivity extends AppCompatActivity {
 
